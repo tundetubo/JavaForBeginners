01fb3113d2a1df37bcc94919c06c5d5f7a249559
Olatunde Tubo <tunde.tubo@gmail.com>
2023-08-23 16:04:53 +0100

    byte and short data

---
+++ Data-Types/ByteData.java
@@ -0,0 +1,10 @@
+public class ByteData {
+	
+	public static void main(String[] args) {
+	 byte eightBitsMinus128 = -128;	
+	 byte eightBits127 = 127;
+         System.out.println("A byte of primitive data can hold whole numbers with a minimum of " + eightBitsMinus128 + " and a maximum of " + eightBits127);		
+	  
+}
+
+}
---
+++ Data-Types/ShortData.java
@@ -0,0 +1,10 @@
+public class ShortData {
+	
+	public static void main(String[] args) {
+	 short sixteenBitsMinus32768  = -32768 ;	
+	 short sixteenBits32767  = 32767 ;
+         System.out.println("two bytes of primitive data can hold whole numbers with a minimum of " + sixteenBitsMinus32768  + " and a maximum of " + sixteenBits32767);		
+	  
+ }
+
+}
