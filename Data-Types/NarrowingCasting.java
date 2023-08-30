496fa31e9921d072fb3cc67a68070df93568e264
Olatunde Tubo <tunde.tubo@gmail.com>
2023-08-30 12:39:40 +0100

    widening and Narrowing Casting

---
+++ Data-Types/NarrowingCasting.java
@@ -0,0 +1,11 @@
+package castingPrimitiveData;
+
+public class NarrowingCasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double thirtyTwoBits  = (int)214748;
+		System.out.println("A double data of a higher value can be casted to "+ thirtyTwoBits + " of primitive data of a lower value" );
+	}
+
+}
---
+++ Data-Types/WideningCasting.java
@@ -0,0 +1,12 @@
+package castingPrimitiveData;
+
+public class WideningCasting {
+
+	public static void main(String[] args) {
+		
+		// 127 can be saved as byte but is casted to int data capable of holding a higher value.
+	    int thirtyTwoBits127 = 127;
+        System.out.println("A byte value "+ thirtyTwoBits127 + " of primitive data can be casted to an int data of higher value" );
+	}
+
+}
